2ca5bdb7b1f41aefb0e704e39a441a0a39ab7dcd
Edidiva <edikanakpan5@gmail.com>
2023-09-30 00:35:36 +0100

    wip

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const videoRoute = require('./routes/videoRoute');
 const dotenv = require('dotenv')
+const swaggerUi = require('swagger-ui-express');
+const swagger= require('./documentation/swagger'); // Import your Swagger file
+
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +15,7 @@ mongoose.connect(process.env.MONGODB_CONNECTION_URL).then(()=> console.log("Data
 // Middleware for JSON parsing and serving static files
 app.use(express.json());
 app.use(express.static('uploads'));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 app.use('/api/videos', videoRoute);
 
 // Start the server
