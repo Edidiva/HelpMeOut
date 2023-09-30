75a83f63ae36ff04988858046273a4e0b3006e32
Edidiva <edikanakpan5@gmail.com>
2023-09-30 18:48:32 +0100

    wip

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 app.use('/api/videos', videoRoute);
 app.use(
   cors({
-    origin: ["http://127.0.0.1:4000", "http://localhost:4000", "*"],
+    origin: ["http://127.0.0.1:3000", "http://localhost:3000", "*"],
     credentials: true,
   })
 );
